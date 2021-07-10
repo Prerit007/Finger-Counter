fe0494b3718146e4a2e30e03a7c26229373a26bb
Prerit007 <75621370+Prerit007@users.noreply.github.com>
2021-07-11 00:26:00 +0530

    1st Commit

---
+++ main.py
@@ -0,0 +1,68 @@
+# Media-Pipe solutions for hand which helps in understanding finger detection
+import cv2
+import time
+import os
+import HandTrackingModule as htm
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+folderPath = "FingImg"
+myList = os.listdir(folderPath)
+print(myList)
+overlayList = []
+for imPath in myList:
+    image = cv2.imread(f'{folderPath}/{imPath}')
+    # print(f'{folderPath}/{imPath}')
+    overlayList.append(image)
+
+print(len(overlayList))
+pTime = 0
+
+detect = htm.handDetector(detectionCon=0.75)
+
+tipIds = [4, 8, 12, 16, 20]
+
+while True:
+    success, img = cap.read()
+    img = detect.findHands(img)
+    lmList = detect.findPosition(img, draw=False)
+    # print(lmList)
+
+    if len(lmList) != 0:
+        fingers = []
+
+        # Thumb
+        if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        # 4 Fingers
+        for id in range(1, 5):
+            if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        # print(fingers)
+        totalFingers = fingers.count(1)
+        print(totalFingers)
+
+        h, w, c = overlayList[totalFingers - 1].shape
+        img[0:h, 0:w] = overlayList[totalFingers - 1]
+
+        cv2.rectangle(img, (20, 225), (170, 425), (100, 255, 64), cv2.FILLED)
+        cv2.putText(img, str(totalFingers), (45, 375), cv2.FONT_HERSHEY_COMPLEX,
+                    6, (210, 208, 64), 25)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, f'FPS: {int(fps)}', (400, 70), cv2.FONT_HERSHEY_COMPLEX,
+                1, (210, 208, 64), 3)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
